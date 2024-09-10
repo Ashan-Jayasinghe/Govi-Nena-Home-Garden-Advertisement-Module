557a6c2d43187b001e05d47934dea590164b9386
AshanJR01 <ashanjr@outlook.com>
2024-09-10 14:30:00 +0530

    Tubers advertisement listing page - frontend did some changes

--- src/app/pages/planting-materials-tubers/planting-materials-tubers.page.ts
+++ src/app/pages/planting-materials-tubers/planting-materials-tubers.page.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-planting-materials-tubers',
@@ -6,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planting-materials-tubers.page.scss'],
 })
 export class PlantingMaterialsTubersPage implements OnInit {
+  tuberForm: FormGroup;
+  images: File[] = [];
+  imageUrls: string[] = [];
+  prices: { label: string, amount: string }[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.tuberForm = this.fb.group({
+      type: ['', Validators.required],
+      variety: ['', Validators.required],
+      title: ['', [Validators.required, Validators.maxLength(50)]],
+      stock: ['', Validators.required],
+      description: [''],
+      priceFor: ['1kg', Validators.required],
+      price: ['', Validators.required],
+      address: ['', Validators.required],
+      mobile: ['', Validators.required],
+      terms: [false, Validators.requiredTrue],
+      specifications: this.fb.array([]),
+    });
   }
 
+  ngOnInit() {}
+
+  onFileChange(event: any) {
+    const files = event.target.files;
+    this.images = Array.from(files);
+    this.imageUrls = this.images.map(file => URL.createObjectURL(file));
+  }
+
+  addPrice() {
+    const priceFor = this.tuberForm.get('priceFor')?.value || '';
+    const price = this.tuberForm.get('price')?.value || '';
+    this.prices.push({ label: priceFor, amount: price });
+    this.tuberForm.patchValue({ price: '' });
+  }
+
+  onSubmit() {
+    if (this.tuberForm.valid) {
+      console.log(this.tuberForm.value);
+      // handle form submission here
+    }
+  }
+
+  get specificationsFormArray(): FormArray {
+    return this.tuberForm.get('specifications') as FormArray;
+  }
+
+  addNewSpecification() {
+    this.specificationsFormArray.push(this.fb.control(''));
+  }
 }
