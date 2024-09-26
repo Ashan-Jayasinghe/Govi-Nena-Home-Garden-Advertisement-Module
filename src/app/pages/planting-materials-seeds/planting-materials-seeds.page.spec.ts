8858f00bb2c3fcfadf5bd0faf6ba7d295982e97c
Denuwan Sankalpa <143308121+Denuwan99@users.noreply.github.com>
2024-09-26 11:40:11 +0530

    update seeds

--- src/app/pages/planting-materials-seeds/planting-materials-seeds.page.spec.ts
+++ src/app/pages/planting-materials-seeds/planting-materials-seeds.page.spec.ts
@@ -15,3 +15,33 @@ describe('PlantingMaterialsSeedsPage', () => {
     expect(component).toBeTruthy();
   });
 });
+
+export class PlantingMaterialsSeedsPage{
+  type: any;
+  variety: any;
+  title: any;
+  images: any;
+  stock: any;
+  specifications: any;
+  price1: any;
+  price5: any;
+  price10: any;
+  address: any;
+  mobile: any;
+  constructor(){}
+
+  addSeeeds(){
+    let data = (
+      this.type,
+      this.variety,
+      this.title,
+      this.images,
+      this.stock,
+      this.specifications,
+      this.price1,
+      this.price5,
+      this.price10,
+      this.address,
+      this.mobile);
+  }
+}
